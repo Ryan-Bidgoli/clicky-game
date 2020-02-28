a6a873c4215b85d7b74d0e8752a31aa4a429743d
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-02-27 21:58:20 -0800

    moved to root directory

---
+++ build/precache-manifest.1264bee95dcf2db1bb896f977eb5cd79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51257ed41c397f02aae9ab0b8755bbe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d04073ff75f716af05d2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "912ffa6b2273aa3d7777",
+    "url": "/static/js/2.2b2159e5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2b2159e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04073ff75f716af05d2",
+    "url": "/static/js/main.27b5fe16.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    scores: 0,
+    topScores: 0,
+    originalFriends: friends
+  };
+
+ componentDidMount(){
+  let newfriends =  this.state.friends.map(friend=>{
+       friend.clicked = false
+
+       return friend
+     })   
+
+     this.setState({
+       friends:newfriends,
+         originalFriends: newfriends  
+     })
+ }
+
+  removeFriend = id => {
+    
+   
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    let newfriends = this.state.friends.map(friend => {
+         
+     
+      if(id ===friend.id){
+
+        if(friend.clicked === false){
+          
+          friend.clicked = true
+          this.setState({
+            scores: this.state.scores + 1
+            
+          })
+
+          if(this.state.scores >= this.state.topScores){
+            this.setState({
+              topScores:this.state.topScores + 1
+            })
+          }
+        }
+        else{
+          this.setState({
+            scores:0
+          })
+        }
+       
+
+      }
+
+      return friend
+    });
+
+    if(this.state.scores === 0){
+       newfriends = this.state.friends.map(friend=>
+        {
+          friend.clicked = false
+           return friend
+        })
+    }      
+
+     newfriends.sort( () => Math.random() - 0.5) ;
+
+     
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends: newfriends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List  Scores{this.state.scores}   topScores{this.state.topScores}  </Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
+
